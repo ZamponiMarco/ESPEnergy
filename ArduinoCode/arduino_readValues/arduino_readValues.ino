1e9fe77fae1e12adf95921edf30b8e3c3d345418
ZamponiMarco <zamponi.marco3@gmail.com>
2021-11-22 15:13:09 +0100

    [UPDATE] Working example in ESP32

--- ArduinoCode/arduino_readValues/arduino_readValues.ino
+++ ArduinoCode/arduino_readValues/arduino_readValues.ino
@@ -1,64 +1,71 @@
-#include <Arduino_FreeRTOS.h>
-#include <timers.h>
-#include <task.h>
-#include <queue.h>
-
 #include "measurement.h"
 
-int volt;
-int ampere;
 TimerHandle_t timer;
 QueueHandle_t queue;
 TaskHandle_t taskConsumer;
-TaskHandle_t taskProducer;
+BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+
 void setup()
 {
-  Serial.begin(9600);
+  Serial.begin(115200);
   pinMode(A5, INPUT);
   pinMode(A4, INPUT);
-  
-  queue = xQueueCreate(100, sizeof(measurement));
-  Serial.println("start");
-  //xTaskCreate(valueConsumer,"consumer",200, (void *)0, tskIDLE_PRIORITY, &taskConsumer);
-  xTaskCreate(valueProducer,"producer",200, (void *)0, 1, &taskProducer);
-  vTaskStartScheduler();
+
+  Serial.println("Starting Program");
+
+  queue = xQueueCreate(10, sizeof(measurement));
+
+  if (queue == NULL) {
+    Serial.println("Couldn't create Queue");
+    ESP.restart();
+  }
+
+  timer = xTimerCreate("Timer", 1000, pdTRUE, (void *)0, pippo);
+
+  if (timer == NULL) {
+    Serial.println("Couldn't create Timer");
+    ESP.restart();
+  }
+
+  xTimerStartFromISR(timer, (BaseType_t*)0);
+  xTaskCreate(valueConsumer, "consumer", 1024, ( void * ) 1, tskIDLE_PRIORITY, &taskConsumer);
+
+  if (taskConsumer == NULL) {
+    Serial.println("Couldn't create Task");
+    ESP.restart();
+  }
 }
 
 void loop()
 {
 }
 
-void valueProducer(void *pvParameters)
-{
-  timer = xTimerCreate("Timer", pdMS_TO_TICKS(1000), pdFALSE, (void *)0, pippo);
-  xTimerStartFromISR(timer, (BaseType_t*)0);
-}
 
-
-void pippo(TimerHandle_t xTimer){
-  volt = analogRead(A5);
-  ampere = analogRead(A4);
-  measurement misura = {volt, ampere, 0};
-  xQueueSendFromISR(queue, &misura, (BaseType_t*)0);
-  Serial.println("Timer work?");
+void pippo(TimerHandle_t xTimer) {
+  struct measurement misura;
+  if (xQueueSendFromISR(queue, &misura, &xHigherPriorityTaskWoken )) {
+    Serial.println("Object sent");
+  } else {
+    Serial.println("Queue is full");
+  }
 }
 
 /* Task to be created. */
 void valueConsumer(void *pvParameters)
 {
-  measurement *pointerMeasurement;
-  // configASSERT( ( ( uint32_t ) pvParameters ) == 1 );
+  configASSERT( ( ( uint32_t ) pvParameters ) == 1 );
+  struct measurement received;
   for (;;)
   {
-    Serial.println("for");
-    /* Task code goes here. */
-    if (xQueueReceive(queue, pointerMeasurement, pdMS_TO_TICKS(100)))
+    if (xQueueReceive(queue, &received, ( TickType_t ) 1000) == pdPASS)
     {
-      //Serial.print("volt: ");
-      //Serial.print(pointerMeasurement->volt);
-      //Serial.print(", ampere: ");
-      //Serial.print(pointerMeasurement->ampere);
-      //Serial.println("");
+      Serial.print("Object received: {volt: ");
+      Serial.print(received.volt);
+      Serial.print(", ampere: ");
+      Serial.print(received.ampere);
+      Serial.println("}");
+    } else {
+      Serial.println("queue empty");
     }
   }
 }
