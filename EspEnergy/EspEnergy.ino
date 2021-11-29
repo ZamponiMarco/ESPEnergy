833a2e3e5bd725f74111f61874520743c2799776
ZamponiMarco <zamponi.marco3@gmail.com>
2021-11-29 23:34:54 +0100

    [ADD] Added code for web server

--- EspEnergy/EspEnergy.ino
+++ EspEnergy/EspEnergy.ino
@@ -1,3 +1,8 @@
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <WebServer.h>
+#include <ESPmDNS.h>
+
 #include "measurement.h"
 
 #define VOLT_PIN 35
@@ -5,49 +10,95 @@
 #define AMPERE_TWO_PIN 33
 #define AMPERE_THREE_PIN 25
 
+WebServer server(80);
+
+const char* ssid = "POCO M3";
+const char* password = "culocane";
+
 TimerHandle_t timer;
 QueueHandle_t queue;
 TaskHandle_t taskConsumer;
 BaseType_t xHigherPriorityTaskWoken = pdFALSE;
 
+boolean startWebServer() {
+  Serial.println("Starting web server...");
+
+  server.on("/", handleRoot);
+
+  server.begin();
+  Serial.println("HTTP server started");
+}
+
+void handleRoot() {
+  server.send(200, "text/plain", "hello from esp32!");
+}
+
+
 void setup()
 {
   Serial.begin(115200);
-  pinMode(VOLT_PIN, INPUT);
-  pinMode(AMPERE_ONE_PIN, INPUT);
-  pinMode(AMPERE_TWO_PIN, INPUT);
-  pinMode(AMPERE_THREE_PIN, INPUT);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  if (MDNS.begin("esp32")) {
+    Serial.println("MDNS responder started");
+  }
 
   Serial.println("Starting Program");
 
-  queue = xQueueCreate(10, sizeof(measurement));
+  Serial.println("Configuration file doesn't exists\nStarting captive portal...");
+  startWebServer();
 
-  if (queue == NULL) {
+  /*
+
+    pinMode(VOLT_PIN, INPUT);
+    pinMode(AMPERE_ONE_PIN, INPUT);
+    pinMode(AMPERE_TWO_PIN, INPUT);
+    pinMode(AMPERE_THREE_PIN, INPUT);
+
+    queue = xQueueCreate(10, sizeof(measurement));
+
+    if (queue == NULL) {
     Serial.println("Couldn't create Queue");
     ESP.restart();
-  }
+    }
 
-  timer = xTimerCreate("Timer", 1000, pdTRUE, (void *)0, readTask);
+    timer = xTimerCreate("Timer", 1000, pdTRUE, (void *)0, readTask);
 
-  if (timer == NULL) {
+    if (timer == NULL) {
     Serial.println("Couldn't create Timer");
     ESP.restart();
-  }
+    }
 
-  xTimerStartFromISR(timer, (BaseType_t*)0);
-  xTaskCreate(valueConsumer, "consumer", 1024, ( void * ) 1, tskIDLE_PRIORITY, &taskConsumer);
+    xTimerStartFromISR(timer, (BaseType_t*)0);
+    xTaskCreate(valueConsumer, "consumer", 1024, ( void * ) 1, tskIDLE_PRIORITY, &taskConsumer);
 
-  if (taskConsumer == NULL) {
+    if (taskConsumer == NULL) {
     Serial.println("Couldn't create Task");
     ESP.restart();
-  }
+    }
+  */
 }
 
 void loop()
 {
+  server.handleClient();
+  delay(2);//allow the cpu to switch to other tasks
 }
 
 
+/*
 void readTask(TimerHandle_t xTimer) {
   int volt = analogRead(VOLT_PIN);
   int ampere_one = analogRead(AMPERE_ONE_PIN);
@@ -61,7 +112,6 @@ void readTask(TimerHandle_t xTimer) {
   }
 }
 
-/* Task to be created. */
 void valueConsumer(void *pvParameters)
 {
   configASSERT( ( ( uint32_t ) pvParameters ) == 1 );
@@ -86,3 +136,5 @@ void valueConsumer(void *pvParameters)
     }
   }
 }
+
+*/
---
+++ EspEnergy/measurement.h
@@ -0,0 +1,11 @@
+#ifndef measurement_h
+#define measurement_h
+
+struct measurement {
+  int volt;
+  int ampere1;
+  int ampere2;
+  int ampere3;
+  int timestamp;
+};
+#endif
